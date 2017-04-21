c9dd040624b469be17810c3b4f7fe68c99a99710
Steven Xu <hanstxu@gmail.com>
2017-04-21 23:53:38 +0000

    Stored command-line arguments into local variables

--- server.cpp
+++ server.cpp
@@ -1,8 +1,17 @@
 #include <string>
 #include <thread>
 #include <iostream>
+using namespace std;
 
-int main()
-{
-  std::cerr << "server is not implemented yet" << std::endl;
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		cerr << "Error: You must give two arguments when initializing the server." << endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	int port_num = stoi(argv[1]);
+	string directory_name = argv[2];
+	
+	cout << port_num << endl;
+	cout << directory_name << endl;
 }
